c9cc4ba98eca616d5385c90e65ac9bcfe9ecad4d
jake <jake@millie.town>
2023-05-16 08:24:31 +0900

    mod, 프로그램 실행

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"github.com/lee-lou2/go/api/restapi"
+	"github.com/lee-lou2/go/apps/notify"
 	"github.com/lee-lou2/go/configs"
 )
 
 func main() {
 	// 환경 변수 조회
 	configs.LoadEnvironments()
-	//// App 실행
-	//{
-	//	notify.Run()
-	//}
-	//restapi.Run()
+	// App 실행
+	{
+		notify.Run()
+	}
+	restapi.Run()
 }
