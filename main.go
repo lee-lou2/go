fb6e7a74241c2658e2181842cfbc595f62afa054
jake <jake@millie.town>
2023-06-29 09:58:33 +0900

    mod, 슬랙 소켓 모두 분리

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/lee-lou2/go/api/restapi"
-	"github.com/lee-lou2/go/apps/socket"
 	"github.com/lee-lou2/go/configs"
 )
 
@@ -12,8 +11,7 @@ func main() {
 
 	// App 실행
 	{
-		// 소켓 모드 추가
-		socket.Run()
+
 	}
 	restapi.Run()
 }
